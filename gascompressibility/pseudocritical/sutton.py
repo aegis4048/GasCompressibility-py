25218e0c07e98bb8e867e7bd56a0a88cdfc2e727
Eric Kim <aegis4048@gmail.com>
2023-06-03 18:55:37 -0500

    sutton codes and tests finished

--- gascompressibility/pseudocritical/sutton.py
+++ gascompressibility/pseudocritical/sutton.py
@@ -19,7 +19,7 @@ class sutton:
         self._check_invalid_mode(self.mode)  # prevent user modification of self.mode
 
         self.sg = None
-        self._T = None
+        self.T_f = None
         self.T = None
         self.P = None
         self.H2S = None
@@ -83,39 +83,49 @@ class sutton:
         self.e_correction = 120 * (self.A ** 0.9 - self.A ** 1.6) + 15 * (self.B ** 0.5 - self.B ** 4)
         return self.e_correction
 
-    def calc_Tpc_corrected(self, sg=None, Tpc=None, e_correction=None, H2S=None, CO2=None):
+    def calc_Tpc_corrected(self, sg=None, Tpc=None, e_correction=None, H2S=None, CO2=None, ignore_conflict=False):
         self._set_first_caller_attributes(inspect.stack()[0][3], locals())
         self._initialize_Tpc(Tpc, sg=sg)
-        self._initialize_e_correction(e_correction, H2S=H2S, CO2=CO2)
+
+        # Correction is not needed if no sour gas is present
+        if e_correction is None and H2S is None and CO2 is None:
+            self.Tpc_corrected = self.Tpc
+            return self.Tpc_corrected
+
+        self._initialize_e_correction(e_correction, H2S=H2S, CO2=CO2, ignore_conflict=ignore_conflict)
         self.Tpc_corrected = self.Tpc - self.e_correction
         return self.Tpc_corrected
 
     """ corrected pseudo-critical pressure (psi)"""
-    def calc_Ppc_corrected(self, sg=None, Tpc=None, Ppc=None, e_correction=None, Tpc_corrected=None, H2S=None, CO2=None):
+    def calc_Ppc_corrected(self, sg=None, Tpc=None, Ppc=None, e_correction=None, Tpc_corrected=None, H2S=None, CO2=None, ignore_conflict=False):
         self._set_first_caller_attributes(inspect.stack()[0][3], locals())
         self._initialize_Ppc(Ppc, sg=sg)
+
+        # Correction is not needed if no sour gas is present
+        if e_correction is None and H2S is None and CO2 is None and Tpc is None and Tpc_corrected is None:
+            self.Ppc_corrected = self.Ppc
+            return self.Ppc_corrected
+
         self._initialize_Tpc(Tpc, sg=sg)
         self._initialize_B(B=None, H2S=H2S)
-        self._initialize_e_correction(e_correction, H2S=H2S, CO2=CO2)
-        self._initialize_Tpc_corrected(Tpc_corrected, sg=sg, Tpc=Tpc, e_correction=e_correction, H2S=H2S, CO2=CO2)
+        self._initialize_e_correction(e_correction, H2S=H2S, CO2=CO2, ignore_conflict=ignore_conflict)
+        self._initialize_Tpc_corrected(Tpc_corrected, sg=sg, Tpc=Tpc, e_correction=e_correction, H2S=H2S, CO2=CO2, ignore_conflict=ignore_conflict)
         self.Ppc_corrected = (self.Ppc * self.Tpc_corrected) / (self.Tpc - self.B * (1 - self.B) * self.e_correction)
         return self.Ppc_corrected
 
     """pseudo-reduced temperature (°R)"""
-    def calc_Tr(self, T=None, Tpc_corrected=None, sg=None, Tpc=None, e_correction=None, H2S=None, CO2=None):
+    def calc_Tr(self, T=None, Tpc_corrected=None, sg=None, Tpc=None, e_correction=None, H2S=None, CO2=None, ignore_conflict=False):
         self._set_first_caller_attributes(inspect.stack()[0][3], locals())
         self._initialize_T(T)
-        self._redundant_argument_check_Tpc_corrected(sg=sg, Tpc=Tpc, H2S=H2S, CO2=CO2, e_correction=e_correction)
-        self._initialize_Tpc_corrected(Tpc_corrected, sg=sg, Tpc=Tpc, e_correction=e_correction, H2S=H2S, CO2=CO2)
+        self._initialize_Tpc_corrected(Tpc_corrected, sg=sg, Tpc=Tpc, e_correction=e_correction, H2S=H2S, CO2=CO2, ignore_conflict=ignore_conflict)
         self.Tr = self.T / self.Tpc_corrected
         return self.Tr
 
     """pseudo-reduced pressure (psi)"""
-    def calc_Pr(self, P=None, Ppc_corrected=None, sg=None, Tpc=None, Ppc=None, e_correction=None, Tpc_corrected=None, H2S=None, CO2=None):
+    def calc_Pr(self, P=None, Ppc_corrected=None, sg=None, Tpc=None, Ppc=None, e_correction=None, Tpc_corrected=None, H2S=None, CO2=None, ignore_conflict=False):
         self._set_first_caller_attributes(inspect.stack()[0][3], locals())
         self._initialize_P(P)
-        self._redundant_argument_check_Ppc_corrected(sg=sg, Ppc=Ppc, Tpc_corrected=Tpc_corrected, Tpc=Tpc, H2S=H2S, CO2=CO2, e_correction=e_correction)
-        self._initialize_Ppc_corrected(Ppc_corrected, sg=sg, Tpc=Tpc, Ppc=Ppc, e_correction=e_correction, Tpc_corrected=Tpc_corrected, H2S=H2S, CO2=CO2)
+        self._initialize_Ppc_corrected(Ppc_corrected, sg=sg, Tpc=Tpc, Ppc=Ppc, e_correction=e_correction, Tpc_corrected=Tpc_corrected, H2S=H2S, CO2=CO2, ignore_conflict=ignore_conflict)
         self.Pr = self.P / self.Ppc_corrected
         return self.Pr
 
@@ -158,10 +168,11 @@ class sutton:
         ) * np.exp(-self.A11 * ((0.27 * self.Pr) / (z * self.Tr)) ** 2) - z
 
     """Newton-Raphson nonlinear solver"""
-    def calc_Z(self, guess=0.9, sg=None, P=None, T=None, Tpc=None, Ppc=None, Tpc_corrected=None, Ppc_corrected=None, H2S=None, CO2=None, Tr=None, Pr=None, e_correction=None, **kwargs):
+    def calc_Z(self, guess=0.9, sg=None, P=None, T=None, Tpc=None, Ppc=None, Tpc_corrected=None, Ppc_corrected=None,
+               H2S=None, CO2=None, Tr=None, Pr=None, e_correction=None, ignore_conflict=False, **kwargs):
         self._set_first_caller_attributes(inspect.stack()[0][3], locals())
-        self._initialize_Pr(Pr, P=P, Ppc_corrected=Ppc_corrected, sg=sg, Tpc=Tpc, Ppc=Ppc, e_correction=e_correction, Tpc_corrected=Tpc_corrected, H2S=H2S, CO2=CO2)
-        self._initialize_Tr(Tr, T, Tpc_corrected=Tpc_corrected, sg=sg, Tpc=Tpc, e_correction=e_correction, H2S=H2S, CO2=CO2)
+        self._initialize_Pr(Pr, P=P, Ppc_corrected=Ppc_corrected, sg=sg, Tpc=Tpc, Ppc=Ppc, e_correction=e_correction, Tpc_corrected=Tpc_corrected, H2S=H2S, CO2=CO2, ignore_conflict=ignore_conflict)
+        self._initialize_Tr(Tr, T, Tpc_corrected=Tpc_corrected, sg=sg, Tpc=Tpc, e_correction=e_correction, H2S=H2S, CO2=CO2, ignore_conflict=ignore_conflict)
         self.Z = optimize.newton(self._calc_Z, guess, **kwargs)
         return self.Z
 
@@ -169,22 +180,39 @@ class sutton:
         """
         Helper function to set properties related to the first function called (first in the call stack).
         This function doesn't do anything for 'calc_...()' functions called inside the first function.
-        For exmaple, if `calc_Ppc_corrected()' is called, this function is passed for 'calc_Ppc()' function which is
+        For exmaple, if `calc_Ppc_corrected()' is called, this function is skipped for 'calc_Ppc()' function which is
         triggered inside `calc_Ppc_corrected()`.
         :param func_name:
-            ex1) "calc_Tpc_corrected",
-            ex2) "calc_Ppc_corrected",
+            ex1) func_name= "calc_Tpc_corrected",
+            ex2) func_name = "calc_Ppc_corrected",
         :param func_kwargs: kwarg parameters passed to 'func_name'
-            ex1) {'self': ...some_string, 'sg': None, 'Tpc': 377.59, 'e_correction': None, 'H2S': 0.07, 'CO2': 0.1}
-            ex2) {'self': ...some_string, 'sg': 0.6, 'Tpc': None, 'e_correction': None, 'H2S': 0.07, 'CO2': 0.1}
+            ex1) func_kwargs = {'self': ...some_string, 'sg': None, 'Tpc': 377.59, 'e_correction': None, 'H2S': 0.07, 'CO2': 0.1}
+            ex2) func_kwargs = {'self': ...some_string, 'sg': 0.6, 'Tpc': None, 'e_correction': None, 'H2S': 0.07, 'CO2': 0.1}
         """
         if not self._first_caller_is_saved:
             func_kwargs = {key: value for key, value in func_kwargs.items() if key != 'self'}
+            if 'ignore_conflict' in func_kwargs:
+
+                if func_kwargs['ignore_conflict'] is False:
+                    """
+                    This modification is needed for self._check_conflicting_arguments(). 
+                    The exception in self._check_conflicting_arguments() compares if "kwarg is not None"
+                    The default 'ignore_conflict' is a boolean object, so comparing if "ignore_conflict is not None"
+                    will raise type error 
+                    """
+                    func_kwargs['ignore_conflict'] = None
+
             self._first_caller_name = func_name
             self._first_caller_kwargs = func_kwargs
             self._first_caller_is_saved = True
         else:
             pass
+
+    def _check_conflicting_arguments(self, func, calculated_var):
+        args = inspect.getfullargspec(func).args[1:]  # arg[0] = 'self'
+        for arg in args:
+            if self._first_caller_kwargs[arg] is not None:
+                raise TypeError('%s() has conflicting keyword arguments "%s" and "%s"' % (self._first_caller_name, calculated_var, arg))
 
     def _initialize_sg(self, sg):
         if sg is None:
@@ -208,6 +236,7 @@ class sutton:
         if T is None:
             raise TypeError("Missing a required argument, T (gas temperature, °F)")
         else:
+            self.T_f = T
             self.T = calc_Fahrenheit_to_Rankine(T)
 
     def _initialize_H2S(self, H2S):
@@ -240,56 +269,53 @@ class sutton:
         if Tpc is None:
             self.calc_Tpc(sg=sg)
         else:
-            self._check_conflicting_arguments(self.calc_Tpc, 'Tpc')
             self.Tpc = Tpc
 
     def _initialize_Ppc(self, Ppc, sg=None):
         if Ppc is None:
             self.calc_Ppc(sg=sg)
         else:
-            self._check_conflicting_arguments(self.calc_Ppc, 'Ppc')
             self.Ppc = Ppc
 
-    def _check_conflicting_arguments(self, func, calculated_var):
-        args = inspect.getfullargspec(func).args[1:]  # arg[0] = 'self'
-        for arg in args:
-            if self._first_caller_kwargs[arg] is not None:
-                raise TypeError('%s() has conflicting keyword arguments "%s" and "%s"' % (self._first_caller_name, calculated_var, arg))
-
-    def _initialize_e_correction(self, e_correction, H2S=None, CO2=None):
+    def _initialize_e_correction(self, e_correction, H2S=None, CO2=None, ignore_conflict=False):
         if e_correction is None:
             self.calc_e_correction(H2S=H2S, CO2=CO2)
         else:
-            self._check_conflicting_arguments(self.calc_e_correction, 'e_correction')
+            if ignore_conflict is False:
+                self._check_conflicting_arguments(self.calc_e_correction, 'e_correction')
             self.e_correction = e_correction
 
-    def _initialize_Tpc_corrected(self, Tpc_corrected, sg=None, Tpc=None, e_correction=None, H2S=None, CO2=None):
+    def _initialize_Tpc_corrected(self, Tpc_corrected, sg=None, Tpc=None, e_correction=None, H2S=None, CO2=None, ignore_conflict=False):
         if Tpc_corrected is None:
-            self.calc_Tpc_corrected(sg=sg, Tpc=Tpc, e_correction=e_correction, H2S=H2S, CO2=CO2)
+            self.calc_Tpc_corrected(sg=sg, Tpc=Tpc, e_correction=e_correction, H2S=H2S, CO2=CO2, ignore_conflict=ignore_conflict)
         else:
-            self._check_conflicting_arguments(self.calc_Tpc_corrected, 'Tpc_corrected')
+            if ignore_conflict is False:
+                self._check_conflicting_arguments(self.calc_Tpc_corrected, 'Tpc_corrected')
             self.Tpc_corrected = Tpc_corrected
 
     def _initialize_Ppc_corrected(self, Ppc_corrected, sg=None, Tpc=None, Ppc=None, e_correction=None,
-                                  Tpc_corrected=None, H2S=None, CO2=None):
+                                  Tpc_corrected=None, H2S=None, CO2=None, ignore_conflict=False):
         if Ppc_corrected is None:
-            self.calc_Ppc_corrected(sg=sg, Tpc=Tpc, Ppc=Ppc, e_correction=e_correction, Tpc_corrected=Tpc_corrected, H2S=H2S, CO2=CO2)
+            self.calc_Ppc_corrected(sg=sg, Tpc=Tpc, Ppc=Ppc, e_correction=e_correction, Tpc_corrected=Tpc_corrected, H2S=H2S, CO2=CO2, ignore_conflict=ignore_conflict)
         else:
-            self._check_conflicting_arguments(self.calc_Ppc_corrected, 'Ppc_corrected')
+            if ignore_conflict is False:
+                self._check_conflicting_arguments(self.calc_Ppc_corrected, 'Ppc_corrected')
             self.Ppc_corrected = Ppc_corrected
 
-    def _initialize_Pr(self, Pr, P=None, Ppc_corrected=None, sg=None, Tpc=None, Ppc=None, e_correction=None, Tpc_corrected=None, H2S=None, CO2=None):
+    def _initialize_Pr(self, Pr, P=None, Ppc_corrected=None, sg=None, Tpc=None, Ppc=None, e_correction=None, Tpc_corrected=None, H2S=None, CO2=None, ignore_conflict=False):
         if Pr is None:
-            self.calc_Pr(P=P, Ppc_corrected=Ppc_corrected, sg=sg, Tpc=Tpc, Ppc=Ppc, e_correction=e_correction, Tpc_corrected=Tpc_corrected, H2S=H2S, CO2=CO2)
+            self.calc_Pr(P=P, Ppc_corrected=Ppc_corrected, sg=sg, Tpc=Tpc, Ppc=Ppc, e_correction=e_correction, Tpc_corrected=Tpc_corrected, H2S=H2S, CO2=CO2, ignore_conflict=ignore_conflict)
         else:
-            self._check_conflicting_arguments(self.calc_Pr, 'Pr')
+            if ignore_conflict is False:
+                self._check_conflicting_arguments(self.calc_Pr, 'Pr')
             self.Pr = Pr
 
-    def _initialize_Tr(self, Tr, T, Tpc_corrected=None, sg=None, Tpc=None, e_correction=None, H2S=None, CO2=None):
+    def _initialize_Tr(self, Tr, T, Tpc_corrected=None, sg=None, Tpc=None, e_correction=None, H2S=None, CO2=None, ignore_conflict=False):
         if Tr is None:
-            self.calc_Tr(T=T, Tpc_corrected=Tpc_corrected, sg=sg, Tpc=Tpc, e_correction=e_correction, H2S=H2S, CO2=CO2)
+            self.calc_Tr(T=T, Tpc_corrected=Tpc_corrected, sg=sg, Tpc=Tpc, e_correction=e_correction, H2S=H2S, CO2=CO2, ignore_conflict=ignore_conflict)
         else:
-            self._check_conflicting_arguments(self.calc_Tr, 'Tr')
+            if ignore_conflict is False:
+                self._check_conflicting_arguments(self.calc_Tr, 'Tr')
             self.Tr = Tr
 
     def _check_missing_P(self, P):
@@ -306,38 +332,6 @@ class sutton:
         if mode != 'sutton' and mode != 'piper':
             raise TypeError("Invalid optional argument, mode (calculation method), input either 'sutton', 'piper', or None (default='sutton')")
         self.mode = mode
-
-    def _redundant_argument_check_Tpc_corrected(self, Tpc_corrected=None, sg=None, Tpc=None, e_correction=None, H2S=None, CO2=None):
-        if Tpc_corrected is not None:
-            if sg is not None:
-                raise TypeError("Redundant arguments Tpc_corrected and sg, input only one of them")
-            if Tpc is not None:
-                raise TypeError("Redundant arguments Tpc_corrected and Tpc, input only one of them")
-            if H2S is not None:
-                raise TypeError("Redundant arguments Tpc_corrected and H2S, input only one of them")
-            if CO2 is not None:
-                raise TypeError("Redundant arguments Tpc_corrected and CO2, input only one of them")
-            if e_correction is not None:
-                raise TypeError("Redundant arguments Tpc_corrected and e_correction, input only one of them")
-
-    def _redundant_argument_check_Ppc_corrected(self, Ppc_corrected=None, sg=None, Tpc=None, Ppc=None, e_correction=None, Tpc_corrected=None, H2S=None, CO2=None):
-        if Ppc_corrected is not None:
-            if Ppc is not None:
-                raise TypeError("Redundant arguments Ppc_corrected and Ppc, input only one of them")
-            if Tpc_corrected is not None:
-                raise TypeError("Redundant arguments Ppc_corrected and Tpc_corrected, input only one of them")
-            self._redundant_argument_check_Tpc_corrected(sg=sg, Tpc=Tpc, H2S=H2S, CO2=CO2, e_correction=e_correction)
-
-    def _redundant_argument_check_Tpc(self, Tpc=None, sg=None):
-        if self.mode == 'piper':
-            if Tpc is not None:
-                if sg is not None:
-                    raise TypeError("Redundant arguments Tpc and sg, input only one of them")
-
-    def _redundant_argument_check_Ppc(self, Ppc=None, Tpc=None, sg=None):
-        if self.mode == 'piper':
-            if Ppc is not None:
-                self._redundant_argument_check_Tpc(Tpc=Tpc, sg=sg)
 
     def quickstart(self):
 
